7f08b4a04206de01862c58917703da809f7af4c5
mantubhavi <mantu.bhavi@gmail.com>
2025-03-29 15:14:43 +0530

    included react, react-dom and parcel modules

--- App.js
+++ App.js
@@ -1,30 +1,16 @@
-/**
- *
- * <div id="parent">
- *  <div id="child1">
- *      <h1>I'm an h1 tag</h1>
- *      <h2></h2>
- *  </div>
- *  <div id="child2">
- *      <h1>I'm an h1 tag</h1>
- *      <h2></h2>
- *  </div>
- * </div>
- *  ReactElement(Object) => HTML(Browser Understands)
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
+  React.createElement("div", { id: "child1", key: 1 }, [
+    React.createElement("h1", { key: 2 }, "I'm an h1 tag"),
+    React.createElement("h2", { key: 3 }, "I'm an h2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
+  React.createElement("div", { id: "child2", key: 4 }, [
+    React.createElement("h1", { key: 5 }, "I'm an h1 tag"),
+    React.createElement("h2", { key: 6 }, "I'm an h2 tag"),
   ]),
 ]);
-
-console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
