9c21c27df1d1fc84f181aab2c13ac86749a42f84
mantubhavi <mantu.bhavi@gmail.com>
2025-04-13 15:17:48 +0530

    created AppLayout, Header, Body and Restro Component and provided basic styling.

--- App.js
+++ App.js
@@ -1,19 +1,86 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-  <>
-    <h1 id="heading">Namasate React using JSX 🚀</h1>
-  </>
-);
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - Restaurant Conatiner
+ *    - Restaurant Card
+ *      - Img
+ *      - Name of Restro, Star Rating, Cusine, Delivery Time
+ * Footer
+ *  - Copryright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
 
-const HeadingComponent = () => (
-  <>
-    <Title />
-    <h1 id="headingComponent">Namasate React using React 🚀</h1>
-  </>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restro-container">
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div
+      className="restro-card"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <img
+        className="restro-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/slheenytwb3iaypqf4yd"
+        alt="Meghana Foods"
+      />
+      <h4>Meghana Foods</h4>
+      <h5>Biryani, North Indian, Asian</h5>
+      <h5>4.4 stars</h5>
+      <h5>40 mins</h5>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
