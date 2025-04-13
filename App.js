d1f43ff21aedc756fae8bb26b913d5cac8cc15be
mantubhavi <mantu.bhavi@gmail.com>
2025-04-13 13:39:09 +0530

    jsx syntax and components.

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1", key: 1 }, [
-    React.createElement("h1", { key: 2 }, "I'm an h1 tag"),
-    React.createElement("h2", { key: 3 }, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2", key: 4 }, [
-    React.createElement("h1", { key: 5 }, "I'm an h1 tag"),
-    React.createElement("h2", { key: 6 }, "I'm an h2 tag"),
-  ]),
-]);
+const Title = () => (
+  <>
+    <h1 id="heading">Namasate React using JSX 🚀</h1>
+  </>
+);
+
+const HeadingComponent = () => (
+  <>
+    <Title />
+    <h1 id="headingComponent">Namasate React using React 🚀</h1>
+  </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingComponent />);
